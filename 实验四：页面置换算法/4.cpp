b55c60434e8a1c1e3890eaa8368e19b55b6daf51
226wyj <308033240@qq.com>
2019-06-12 09:53:53 +0800

    final

--- 实验四：页面置换算法/4.cpp
+++ 实验四：页面置换算法/4.cpp
@@ -25,42 +25,26 @@ int main()
 {
 	/***********initialize variable***********/
 	int i, j, pid, gtid, reid;
-	int access_index = 0;		       		 // the index process access Access_Series
+	int access_index = 0;		       		 // the index of Access_Series
 	int success_flag = false;	       		 // value is true if page exists else false 
 	float diseffect = 0.0;		       		 // pages absence times 
-	float absence_rate = 0.0;	      		 // pages absence Index of access --absence_rate=diseffect/total_instruction 
-	float success_rate = 0.0;	      		 // pages success Index of access --success_rate=1-absence_rate 
-    int Access_Series[total_instruction]; 	 // the pages series for access of memory 
-    one_frame M_Frame[frame_num];  	     	 // memory access series 
-	one_frame M_Frame_FIFO[frame_num_FIFO];  // memory access series FIFO
+	float absence_rate = 0.0;	      		 // absence_rate = diseffect / total_instruction 
+	float success_rate = 0.0;	      		 // success_rate = 1 - absence_rate 
+    int Access_Series[total_instruction]; 	 // random number sequence 
+    one_frame M_Frame[frame_num];  	     	 // memory access sequence 
+	one_frame M_Frame_FIFO[frame_num_FIFO];  // memory access sequence FIFO
 	int Belady[total_instruction] = {2, 7, 2, 3, 6, 3, 10, 7, 4, 5, 2, 6, 5, 10, 7, 6, 8, 5, 8, 10}; // Belady
+
 	/***********initialize memory access series***********/
-	printf("Do you want to see Belady 100%? (1-no  2-yes)\n");
-	do{
-		printf("your choice: ");
-		scanf("%d", &i);
-	}while(i != 1 && i != 2);
-	if(i == 1)	// 普通随机数
-	{
-		srand((int)time(0));		       		 // seed for random number
-		printf("* access series numbers:");
-		for(i = 0; i < total_instruction; i++)
-		{	
-			Access_Series[i] = 1 + (int)(10.0*rand()/(RAND_MAX+1.0));//[1,10]
-			printf("%d ",Access_Series[i]);	
-		}
-		printf("\n");
+	srand((int)time(0));		       		 
+	printf("\n* access series numbers:");
+	for(i = 0; i < total_instruction; i++)
+	{	
+		Access_Series[i] = 1 + (int)(10.0 * rand() / (RAND_MAX + 1.0));	//[1,10]
+		printf("%d ",Access_Series[i]);	
 	}
-	else if(i == 2)	// 特定Belady序列
-	{
-		/***********initialize belady***************************/
-		for(i = 0; i < total_instruction; i++)
-		{
-			Access_Series[i] = Belady[i];
-			printf("%d ", Access_Series[i]);	
-		}
-		printf("\n");
-	}
+	printf("\n\n");
+
 	/***********initialize data structure M_Frame***********/
     for(i = 0; i < frame_num; i++)
 	{	
@@ -73,6 +57,7 @@ int main()
 		M_Frame[i].page_no = 0;
 		M_Frame[i].flag = UNUSED;
 	}
+
 	/**********************subprocess_FIFO**********************/
 	pid = fork();
 
@@ -117,8 +102,8 @@ int main()
 
 				if(i == frame_num)	// full 
 				{
-					for(i = 0; i < frame_num - 1; i++)	
-						M_Frame[i].page_no = M_Frame[i + 1].page_no;			// 栈中元素依次前移
+					for(i = 0; i < frame_num - 1; i++)	 
+						M_Frame[i].page_no = M_Frame[i + 1].page_no;			 // 栈中元素依次前移
 					M_Frame[frame_num - 1].page_no = Access_Series[access_index];// 新元素在栈顶
 				}
 			}
@@ -128,7 +113,7 @@ int main()
 
 		absence_rate = diseffect / total_instruction;	// 缺页率
 		success_rate = 1 - absence_rate;
-		printf("* subprocess_FIFO %d successful [%d]:absence_rate=%f, sbIndex=%f\n\n",
+		printf("* subprocess_FIFO %d successful [%d]:absence_rate=%f, success_rate=%f\n\n",
 			gtid, frame_num, absence_rate, success_rate);
 
 		access_index = 0;
@@ -196,7 +181,7 @@ int main()
 
 		absence_rate=diseffect/total_instruction;
 		success_rate=1-absence_rate;
-		printf("* subprocess_FIFO %d successful [%d]: absence_rate=%f, sbIndex=%f\n\n",
+		printf("* subprocess_FIFO %d successful [%d]: absence_rate=%f, success_rate=%f\n\n",
 			gtid, frame_num_FIFO, absence_rate, success_rate);
 
 		access_index = 0;
@@ -222,7 +207,7 @@ int main()
 
 		while(access_index < total_instruction)
 		{
-			for(i = 0; i < frame_num; i++)	// check if page exists in memory or not 
+			for(i = 0; i < frame_num; i++)	// check if page exists in memory 
 			{	
 				if (M_Frame[i].flag == USED)
 				{
@@ -247,11 +232,11 @@ int main()
 					break;	
 			}
 
-			if(!success_flag)	// page absent/page not exists in memory 
+			if(!success_flag)	// page absent/page doesn't exist in memory 
 			{
 				diseffect += 1.0;
 
-				for(i = 0; i < frame_num; i++)		// check if frames are full or not 
+				for(i = 0; i < frame_num; i++)		// check if frames are full 
 				{	
 					if(M_Frame[i].flag == UNUSED)	// not full 
 					{
@@ -274,7 +259,7 @@ int main()
 				if(M_Frame[i].flag == UNUSED)
 					printf("* M_Frame[%d]:  \n",i);
 				else
-					printf("* M_Frame[%d]: %d\n",i,M_Frame[i].page_no);
+					printf("* M_Frame[%d]: %d\n", i, M_Frame[i].page_no);
 			}
 			printf("--------------\n");
 			success_flag = false;
@@ -283,7 +268,7 @@ int main()
 
 		absence_rate = diseffect / total_instruction;
 		success_rate = 1 - absence_rate;
-		printf("* subprocess_LRU %d successful [%d]: absence_rate=%f, sbIndex=%f\n", 
+		printf("* subprocess_LRU %d successful [%d]: absence_rate=%f, success_rate=%f\n", 
 			gtid, frame_num, absence_rate, success_rate);
 		exit(0);
 	}
