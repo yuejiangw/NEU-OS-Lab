8829f8a2d12d5deb096d9b85e165306ee2ecb992
226wyj <308033240@qq.com>
2019-06-12 15:12:43 +0800

    final

--- 实验四：页面置换算法/4.cpp
+++ 实验四：页面置换算法/4.cpp
@@ -26,22 +26,38 @@ int main()
 	/***********initialize variable***********/
 	int i, j, pid, gtid, reid;
 	int access_index = 0;		       		 // the index of Access_Series
-	int success_flag = false;	       		 // value is true if page exists else false 
-	float diseffect = 0.0;		       		 // pages absence times 
+	int success_flag = false;	       		 // value is true if page exists 
+	float diseffect = 0.0;		       		 // pages absence counts 
 	float absence_rate = 0.0;	      		 // absence_rate = diseffect / total_instruction 
 	float success_rate = 0.0;	      		 // success_rate = 1 - absence_rate 
     int Access_Series[total_instruction]; 	 // random number sequence 
     one_frame M_Frame[frame_num];  	     	 // memory access sequence 
 	one_frame M_Frame_FIFO[frame_num_FIFO];  // memory access sequence FIFO
 	int Belady[total_instruction] = {2, 7, 2, 3, 6, 3, 10, 7, 4, 5, 2, 6, 5, 10, 7, 6, 8, 5, 8, 10}; // Belady
-
+	int Equal[total_instruction] = {5, 1, 10, 2, 5, 1, 2, 2, 1, 10, 10, 6, 7, 2, 8, 5, 4, 8, 8, 8};
 	/***********initialize memory access series***********/
-	srand((int)time(0));		       		 
-	printf("\n* access series numbers:");
-	for(i = 0; i < total_instruction; i++)
-	{	
-		Access_Series[i] = 1 + (int)(10.0 * rand() / (RAND_MAX + 1.0));	//[1,10]
-		printf("%d ",Access_Series[i]);	
+	printf("Do you want to see Belady? (1-yes  2-no)\n");
+	do{
+		printf("Your choice: ");
+		scanf("%d", &i);
+	}while(i != 1 && i != 2);
+	if(i == 1)
+	{
+		for(int k = 0; k < total_instruction; k++)
+		{
+			Access_Series[k] = Belady[k];
+			printf("%d ",Access_Series[k]);	
+		}
+	}
+	else if(i == 2)
+	{
+		srand((int)time(0));		       		 
+		printf("\n* access series numbers:");
+		for(int k = 0; k < total_instruction; k++)
+		{	
+			Access_Series[k] = 1 + (int)(10.0 * rand() / (RAND_MAX + 1.0));	//[1,10]
+			printf("%d ",Access_Series[k]);	
+		}
 	}
 	printf("\n\n");
 
@@ -52,10 +68,10 @@ int main()
 		M_Frame[i].flag = UNUSED;
 	}
 
-	for(i=0;i<frame_num_FIFO;i++)
+	for(i = 0; i < frame_num_FIFO; i++)
 	{	
-		M_Frame[i].page_no = 0;
-		M_Frame[i].flag = UNUSED;
+		M_Frame_FIFO[i].page_no = 0;
+		M_Frame_FIFO[i].flag = UNUSED;
 	}
 
 	/**********************subprocess_FIFO**********************/
@@ -72,7 +88,7 @@ int main()
 
 		while(access_index < total_instruction)
 		{
-			for(i = 0; i < frame_num; i++)		// check if page exists in memory or not 
+			for(i = 0; i < frame_num; i++)		// check if page exists in memory 
 			{	
 				if (M_Frame[i].flag == USED)
 				{
@@ -82,7 +98,7 @@ int main()
 						break;
 					}
 				}
-				else
+				else							// memory isn't full
 					break;
 			}
 
@@ -90,7 +106,7 @@ int main()
 			{
 				diseffect += 1.0;
 
-				for(i = 0; i < frame_num; i++)		// check if frames are full or not 
+				for(i = 0; i < frame_num; i++)		// check if frames are full 
 				{	
 					if(M_Frame[i].flag == UNUSED)	// not full 
 					{
@@ -107,11 +123,20 @@ int main()
 					M_Frame[frame_num - 1].page_no = Access_Series[access_index];// 新元素在栈顶
 				}
 			}
+
+			// for(i = frame_num - 1; i >= 0; i--)	// print the state of frames 
+			// {
+			// 	if(M_Frame[i].flag == UNUSED)
+			// 		printf("* M_Frame[%d]:  \n",i);
+			// 	else
+			// 		printf("* M_Frame[%d]: %d\n", i, M_Frame[i].page_no);
+			// }
+			// printf("--------------\n");
 			success_flag = false;
 			access_index++;
 		}
 
-		absence_rate = diseffect / total_instruction;	// 缺页率
+		absence_rate = diseffect / total_instruction;	// 缺页率计算
 		success_rate = 1 - absence_rate;
 		printf("* subprocess_FIFO %d successful [%d]:absence_rate=%f, success_rate=%f\n\n",
 			gtid, frame_num, absence_rate, success_rate);
@@ -169,18 +194,25 @@ int main()
 
 				if(i == frame_num_FIFO)	// full 
 				{
-					for(i=0;i<frame_num_FIFO - 1;i++)	
+					for(i = 0; i < frame_num_FIFO - 1; i++)	
 						M_Frame_FIFO[i].page_no = M_Frame_FIFO[i + 1].page_no;
 					M_Frame_FIFO[frame_num_FIFO - 1].page_no = Access_Series[access_index];
 				}
 			}
-
-			success_flag=false;
+			// for(i = frame_num_FIFO - 1; i >= 0; i--)	// print the state of frames 
+			// {
+			// 	if(M_Frame_FIFO[i].flag == UNUSED)
+			// 		printf("* M_Frame[%d]:  \n",i);
+			// 	else
+			// 		printf("* M_Frame[%d]: %d\n", i, M_Frame_FIFO[i].page_no);
+			// }
+			// printf("--------------\n");
+			success_flag = false;
 			access_index++;
 		}
 
-		absence_rate=diseffect/total_instruction;
-		success_rate=1-absence_rate;
+		absence_rate = diseffect / total_instruction;
+		success_rate = 1 - absence_rate;
 		printf("* subprocess_FIFO %d successful [%d]: absence_rate=%f, success_rate=%f\n\n",
 			gtid, frame_num_FIFO, absence_rate, success_rate);
 
@@ -215,7 +247,7 @@ int main()
 					{
 						success_flag = true;	// page exists in memory 
 
-						for(j = i; j < frame_num - 1; j++)	// reanrange the sequence of page in frames 
+						for(j = i; j < frame_num - 1; j++)	// 将命中的页重新放到栈顶
 						{
 							if (M_Frame[j + 1].flag == UNUSED)
 								break;
@@ -232,7 +264,7 @@ int main()
 					break;	
 			}
 
-			if(!success_flag)	// page absent/page doesn't exist in memory 
+			if(!success_flag)	// 未命中 
 			{
 				diseffect += 1.0;
 
