09efe9b7b94bd7bcfa8c40668905464c692865d5
Wu <308033240@qq.com>
2024-10-12 21:27:58 -0700

    format parenthesis

--- 实验二：生产者消费者/2.cpp
+++ 实验二：生产者消费者/2.cpp
@@ -15,19 +15,15 @@ int writeptr, readptr;			 // 写指针　读指针
 int consumer, producer;			 // 消费者等待数量　生产者等待数量
 
 // 初始化
-void init()
-{
-	for (int i = 0; i < BUFFER_SIZE; ++i)
-	{
-		buffer[i] = 0;
-	}
-	while (!pQueue.empty())
-	{
-		pQueue.pop();
-	}
-	while (!cQueue.empty())
-	{
-		cQueue.pop();
+void init() {
+    for (int i = 0; i < BUFFER_SIZE; ++i) {
+        buffer[i] = 0;
+    }
+    while (!pQueue.empty()) {
+        pQueue.pop();
+    }
+    while (!cQueue.empty()) {
+        cQueue.pop();
 	}
 	data = 0;
 	writeptr = readptr = 0;
@@ -35,12 +31,9 @@ void init()
 }
 
 // 查找空位
-int findEmpty(vector<int>& v, int len)
-{
-	for (int i = 0; i < len; ++i)
-	{
-		if (v[i] == 0)
-		{
+int findEmpty(vector<int>& v, int len) {
+	for (int i = 0; i < len; ++i) {
+		if (v[i] == 0) {
 			return i;
 		}
 	}
@@ -48,12 +41,9 @@ int findEmpty(vector<int>& v, int len)
 }
 
 // 查找脏位
-int findDirty(vector<int>& v, int len)
-{
-	for (int i = 0; i < len; ++i)
-	{
-		if (v[i] > 0)
-		{
+int findDirty(vector<int>& v, int len) {
+	for (int i = 0; i < len; ++i) {
+		if (v[i] > 0) {
 			return i;
 		}
 	}
@@ -61,12 +51,9 @@ int findDirty(vector<int>& v, int len)
 }
 
 // 缓冲区是否满
-bool isFull(vector<int>& v, int len)
-{
-	for (int i = 0; i < len; i++)
-	{
-		if (v[i] == 0)
-		{
+bool isFull(vector<int>& v, int len) {
+	for (int i = 0; i < len; i++) {
+		if (v[i] == 0) {
 			return false;
 		}
 	}
@@ -74,12 +61,9 @@ bool isFull(vector<int>& v, int len)
 }
 
 // 缓冲区是否空
-bool isEmpty(vector<int>& v, int len)
-{
-	for (int i = 0; i < len; i++)
-	{
-		if (v[i] != 0)
-		{
+bool isEmpty(vector<int>& v, int len) {
+	for (int i = 0; i < len; i++) {
+		if (v[i] != 0) {
 			return false;
 		}
 	}
@@ -87,77 +71,57 @@ bool isEmpty(vector<int>& v, int len)
 }
 
 // 遍历展示队列中的内容
-void showQueue(queue<int> q)
-{
+void showQueue(queue<int> q) {
 	int len = q.size();
-	for (int i = 0; i < len; i++)
-	{
+	for (int i = 0; i < len; i++) {
 		cout << q.front() << "	";
 		q.pop();
 	}
 }
 
 // 显示缓冲区内容
-void show()
-{
-	for (int i = 0; i < BUFFER_SIZE; ++i)
-	{
-		if (buffer[i] > 0)
-		{
+void show() {
+	for (int i = 0; i < BUFFER_SIZE; ++i) {
+		if (buffer[i] > 0) {
 			cout << "	" << buffer[i] << "   |";
-		}
-		else
-		{
+		} else {
 			cout << "      |";
 		}
 	}
 	cout << "\nwriteptr = " << writeptr << "\treadptr = " << readptr << "\t";
 
 	// 打印生产者
-	if (producer)
-	{
+	if (producer) {
 		cout << "\nPRODUCER wait : " << producer << "\n"
 			<< "pQueue: ";
 		showQueue(pQueue);
-	}
-	else
-	{
+	} else {
 		cout << "\nPRODUCER ready\t";
 	}
 
 	// 打印消费者
-	if (consumer)
-	{
+	if (consumer) {
 		cout << "\nCONSUMER wait : " << consumer;
-	}
-	else
-	{
+	} else {
 		cout << "\nCONSUMER ready";
 	}
 	cout << endl;
 }
 
 // 生产
-void produce()
-{
-	if (findEmpty(buffer, BUFFER_SIZE) == -1)
-	{
+void produce() {
+	if (findEmpty(buffer, BUFFER_SIZE) == -1) {
 		// 缓冲区满,阻塞
 		++data;
 		producer++;
 		pQueue.push(data);
-	}
-	else if (isEmpty(buffer, BUFFER_SIZE))
-	{
+	} else if (isEmpty(buffer, BUFFER_SIZE)) {
 		// 缓冲区空
-		if (cQueue.empty())
-		{
+		if (cQueue.empty()) {
 			// 消费者等待队列为空,直接写入即可
 			data++;
 			buffer[writeptr] = data;
-		}
-		else
-		{
+		} else {
 			// 有阻塞的消费者，先写入再读出
 			data++;
 			buffer[writeptr] = data;
@@ -169,9 +133,7 @@ void produce()
 			cQueue.pop();
 		}
 		writeptr = (writeptr + 1) % BUFFER_SIZE;
-	}
-	else
-	{
+	} else {
 		// 缓冲区不空也不满,直接写入即可
 		data++;
 		buffer[writeptr] = data;
@@ -180,19 +142,14 @@ void produce()
 }
 
 // 消费
-void consume()
-{
-	if (findDirty(buffer, BUFFER_SIZE) == -1)
-	{
+void consume() {
+	if (findDirty(buffer, BUFFER_SIZE) == -1) {
 		// 缓冲区空,则阻塞
 		consumer++;
 		cQueue.push(data);
-	}
-	else if (isFull(buffer, BUFFER_SIZE))
-	{
+	} else if (isFull(buffer, BUFFER_SIZE)) {
 		// 缓冲区满
-		if (!pQueue.empty())
-		{
+		if (!pQueue.empty()) {
 			// 生产者等待队列不空，则先读出再写入
 			buffer[readptr] = 0;
 			readptr = (readptr + 1) % BUFFER_SIZE;
@@ -202,16 +159,12 @@ void consume()
 			writeptr = (writeptr + 1) % BUFFER_SIZE;
 			pQueue.pop();
 			producer--;
-		}
-		else
-		{
+		} else {
 			// 生产者队列为空
 			buffer[readptr] = 0;
 			readptr = (readptr + 1) % BUFFER_SIZE;
 		}
-	}
-	else
-	{
+	} else {
 		// 缓冲区不空也不满,直接读取即可
 		buffer[readptr] = 0;
 		readptr = (readptr + 1) % BUFFER_SIZE;
@@ -219,27 +172,18 @@ void consume()
 }
 
 // 启动程序
-void start()
-{
-	char c;
+void start() {
+    char c;
 	cin >> c;
 	// e = exit    p = produce    c = consume
-	while (c)
-	{
-		if (c == 'e')
-		{
+	while (c) {
+		if (c == 'e'){
 			break;
-		}
-		else if (c == 'p')
-		{
+		} else if (c == 'p') {
 			produce();
-		}
-		else if (c == 'c')
-		{
+		} else if (c == 'c') {
 			consume();
-		}
-		else
-		{
+		} else {
 			cout << "Please enter e(exit), p(produce), or c(consume)." << endl;
 			continue;
 		}
@@ -248,8 +192,7 @@ void start()
 	}
 }
 
-int main()
-{
+int main() {
 	cout << "e-exit    p-produce	c-consume" << endl;
 	init();
 	start();
