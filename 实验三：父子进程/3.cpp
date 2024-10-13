e69838ae32366a14e4b49f14e4a58b6247b2330d
Wu <308033240@qq.com>
2024-10-12 21:29:12 -0700

    format parenthesis

--- 实验三：父子进程/3.cpp
+++ 实验三：父子进程/3.cpp
@@ -10,16 +10,14 @@
 #define F_ULOCK 0 // 解锁
 #define F_LOCK 1  // 互斥锁定区域
 
-int main()
-{
-	int num, fd[2], result = -1;
+int main() {
+    int num, fd[2], result = -1;
 	pid_t pid, reid, gtid;
 	char outpipe[100], father_str[100];
 
 	// 创建管道, fd[0]读管道，fd[1]写管道
 	result = pipe(fd);
-	if (result == -1)
-	{
+	if (result == -1) {
 		printf("fail to create pipe \n");
 		return -1;
 	}
@@ -29,19 +27,15 @@ int main()
 	scanf("%d", &num);
 	printf("\n****************************************************\n");
 
-	while (num > 0)
-	{
+	while (num > 0) {
 		pid = fork();
 		printf("pid: %d\n", pid);
 
-		if (pid == -1)
-		{
+		if (pid == -1) {
 			// 创建失败
 			printf("creat subprocess failed!\n");
 			exit(0);
-		}
-		else if (pid == 0)
-		{
+		} else if (pid == 0) {
 			// 创建子进程，从子进程返回ID
 			printf("now I'm writing in subprocess %d\n", gtid = getpid());
 			lockf(fd[1], F_LOCK, 0); // 加锁
@@ -51,17 +45,12 @@ int main()
 			lockf(fd[1], F_ULOCK, 0); // 解锁
 			printf("send message successfully and exit subprocess %d\n", gtid);
 			exit(0);
-		}
-		else if (pid > 0)
-		{
+		} else if (pid > 0) {
 			// 创建子进程，从父进程返回子进程的ID
 			reid = wait(NULL);
-			if (reid == -1)
-			{
+			if (reid == -1) {
 				printf("father process calls subprocess failed!\n");
-			}
-			else
-			{
+			} else {
 				read(fd[0], father_str, sizeof(father_str));
 				printf("\nfather process reads from subprocess %d:\n", reid);
 				printf("%s\n", father_str);
